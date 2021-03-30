b3a23f3db7241cbf97d6ef2dc7c6a21e9d8b1485
Yuki <yuki_matsubara@outlook.com>
2021-03-30 01:24:18 -0700

    start, stop, reset button implemented

--- js/timer.js
+++ js/timer.js
@@ -3,10 +3,71 @@ const startBtn = document.querySelector(".startBtn");
 const stopBtn = document.querySelector(".stopBtn");
 const resetBtn = document.querySelector(".resetBtn");
 
+const hour = document.querySelector(".hour");
+const min = document.querySelector(".min");
+const second = document.querySelector(".second");
+
+let displayHour = 0;
+let displayMin = 0;
+let displaySec = 0;
+let interval;
+
 /* ====== Function Declaration  ===== */
 
 const startTimer = () => {
+  //second untiil 59
+  console.log("timer started");
+
+  if (displaySec < 60) {
+    //count second up to 59
+    displaySec++;
+
+    if (displaySec < 10) {
+      second.innerHTML = "0" + displaySec;
+    } else {
+      second.innerHTML = displaySec;
+    }
+
+  }
+  //continue from here
+
+  // if (displaySec > 60) {
+  //   //increment min
+  //   displayMin++
+  //   if (displayMin < 10) {
+  //     min.innerHTML = "0" + displayMin;
+  //   } else {
+  //     min.innerHTML = displayMin;
+  //   }
+  // }
+
 
 }
 
-startBtn.addEventListener("click", startTimer());
+const stopTimer = () => {
+  clearInterval(interval);
+}
+
+const resetTimer = () => {
+  clearInterval(interval);
+  hour.innerHTML = "00";
+  min.innerHTML = "00";
+  second.innerHTML = "00";
+}
+
+/* ====== Function Call  ===== */
+startBtn.addEventListener("click", () => {
+  interval = setInterval(startTimer, 1000);
+  startBtn.disabled = true; //disabled : prevent the button from being clicked multiple times
+  console.log(interval);//1
+});
+
+stopBtn.addEventListener("click", () => {
+  stopTimer();
+  startBtn.disabled = false;
+});
+
+resetBtn.addEventListener("click", () => {
+  resetTimer();
+  startBtn.disabled = false;
+});
